eb48d8d23cdcddce43f37c73dfeca7cbc26caba6
Ashton Taylor <ashtonwtaylor@gmail.com>
2026-06-17 17:43:53 -0500

    Added save and load functions to use localStorage for user data. Also made some more formatting + comment changes

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,4 +1,4 @@
-// Store highlights as "YYYY-M-D" strings for easy lookup:
+// Get current date in YYYY-MM-DD format
 let current = new Date();
 
 // Define structure of user data to be exported in JSON:
@@ -9,9 +9,21 @@ let state = {
     pto: { hours: 0 },
     sick: { hours: 0 }
   },
-  // The values for days will be in the format "YYYY-MM-DD": "day type"
+  // The values for days will be in the format "YYYY-MM-DD": "day type":
   days: {}
 };
+
+// Loading/saving user data from local storage:
+function saveState() {
+  localStorage.setItem('ptoData', JSON.stringify(state));
+}
+
+function loadState() {
+  const saved = localStorage.getItem('ptoData');
+  if (saved) {
+    Object.assign(state, JSON.parse(saved));
+  }
+}
 
 
 function renderCalendar() {
@@ -27,7 +39,7 @@ function renderCalendar() {
   });
 
 
-  // Day-of-week headers
+  // Day-of-week headers:
   ['Su','Mo','Tu','We','Th','Fr','Sa'].forEach(d => {
     const el = document.createElement('div');
     el.className = 'day';
@@ -37,7 +49,7 @@ function renderCalendar() {
   });
 
 
-  // Empty cells before the 1st
+  // Empty cells before the 1st:
   const firstDay = new Date(year, month, 1).getDay();
   for (let i = 0; i < firstDay; i++) {
     const el = document.createElement('div');
@@ -46,18 +58,22 @@ function renderCalendar() {
   }
 
 
-  // Day cells
+  // Day cells:
   // Works by getting the last day of the month previous to month plus 1,
   // which gives the number of days in month + 0:
   const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  // Create elements for each day of the month:
   for (let d = 1; d <= daysInMonth; d++) {
     const key = `${year}-${month}-${d}`;
     const el = document.createElement('div');
-    el.className = 'day' + (highlighted.has(key) ? ' highlighted' : '');
+
+    // Append the day type to the class name, if a type exists for that day:
+    el.className = 'day' + (state.days[key] ? ' ' + state.days[key]: '');
+
     el.textContent = d;
     el.addEventListener('click', () => {
-      highlighted.has(key) ? highlighted.delete(key) : highlighted.add(key);
-      renderCalendar(); // re-render to reflect change
+      // TODO: Open popup menu when user clicks on a day
     });
     calendar.appendChild(el);
   }
@@ -75,5 +91,5 @@ document.getElementById('next').addEventListener('click', () => {
   renderCalendar();
 });
 
-
+loadState();
 renderCalendar();
